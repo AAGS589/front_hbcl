b6b4c8096b791f26aa78a583066fd3c25508a1a0
Alonso Anselmo Gómez Sánchez <68868495+AAGS589@users.noreply.github.com>
2022-06-29 11:34:12 -0500

    Se agregaron estilos al formulario de inicio de sesión

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,36 @@
 import React from 'react'
-
+import '../assets/estilos-login.css'
+import {Link} from "react-router-dom";
 
 class Login extends React.Component{
     render(){
         return(
-            <h1>Vista de login</h1>
+            <div class={'body-login'}>
+                <div className={'section-inputs '}>
+                    <h1 id={'titulo-login'}>Iniciar Sesión</h1>
+                    <div className={'formulario'}>
+
+                        <input className={'input inputLogin'} type={'text'}
+                            name={'username'}
+                            id={'username'}
+                            placeholder={'Ingrese su usuario'}/>
+                    </div>
+                    <div className={'formulario'}>
+
+                        <input className={'input inputLogin'} type={'text'}
+                               name={'username'}
+                               id={'username'}
+                               placeholder={'Ingrese su contraseña'}/>
+                    </div>
+                    <div className="formulario">
+                        <Link to='/register'>
+                            <button type="button" className="btn-login">Iniciar sesión</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+
         )
     }
 
